c5517926649a23de33716ca0a0e62a29a44abfbd
ElderL99 <adan.lugo.barrientos@outlook.com>
2025-09-15 19:04:02 -0600

    add: mongoosee and conneccions, .env ready to use

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+require("dotenv").config();
 
+const db = require("./src/lib/Db/db");
+
+db.connect()
+  .then(() => {
+    console.log(`DB connected successfully 🎉`);
+  })
+  .catch((error) => {
+    console.log(`DB connection faild ${error}`);
+  });
