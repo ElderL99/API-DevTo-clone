0b618016770f6ef0f75e782cbec007ac4631590f
ElderL99 <adan.lugo.barrientos@outlook.com>
2025-09-16 15:48:08 -0600

    add helmet and cors to server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 const RouterUser = require("./routes/user.routes");
 const RouterAuth = require("../src/routes/auth.routes");
 const RouterPost = require("./routes/post.routes");
 const app = express();
 
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
 
 app.use("/post", RouterPost);
